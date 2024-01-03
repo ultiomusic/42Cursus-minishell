10214376a5bafe2f373ad6c52d78bcd744e96234
Ultio <94832543+ultiomusic@users.noreply.github.com>
2024-01-03 14:16:00 +0300

    Add files via upload

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,62 +3,94 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: baer <baer@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: beeligul <beeligul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/03 16:03:39 by baer              #+#    #+#             */
-/*   Updated: 2023/08/03 17:09:27 by baer             ###   ########.fr       */
+/*   Created: 2024/01/02 18:56:48 by beeligul          #+#    #+#             */
+/*   Updated: 2024/01/02 18:56:48 by beeligul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
-static	char	*ft_strndup(char *prev, char *next)
+void	*ft_realloc(void *ptr, size_t size)
 {
-	char	*arr;
-	char	*temp;
+	void	*new_ptr;
+	int		i;
 
-	arr = (char *)malloc(sizeof(char) * (next - prev + 1));
-	temp = arr;
-	while (prev < next)
-		*arr++ = *prev++;
-	*arr = '\0';
-	return (temp);
+	if (ptr == NULL)
+		return (malloc(size));
+	if (!size)
+		return (ptr);
+	new_ptr = malloc(size);
+	ft_memcpy(new_ptr, ptr, size);
+	i = 0;
+	free(ptr);
+	return (new_ptr);
 }
 
-char	*ft_skipnext(char *next, char *c)
+int	is_space_or_tab(char c)
 {
-	if (*next == '"' || *next == '\'')
+	return (c == ' ' || c == '\t');
+}
+
+void	quote_func(const char **s, char *c)
+{
+	int	in_quote;
+
+	in_quote = 0;
+	while (**s)
 	{
-		next++;
-		ft_quotemove(&next);
-		next++;
+		if (**s == '"' || **s == '\'')
+		{
+			if (!in_quote)
+				in_quote = **s;
+			else if (in_quote == **s)
+				in_quote = 0;
+		}
+		else if (in_quote)
+			;
+		else if (is_space_or_tab(**s) || ft_strchr(c, **s))
+			break ;
+		(*s)++;
 	}
-	else
-		next = move(next, c);
-	return (next);
+}
+
+void	put_argument(char ***arr, int *arr_size, int length, const char *start)
+{
+	if (length > 0)
+	{
+		*arr = ft_realloc(*arr, (*arr_size + 1) * sizeof(char *));
+		(*arr)[*arr_size] = malloc(length + 1);
+		ft_strncpy((*arr)[*arr_size], start, length);
+		(*arr)[*arr_size][length] = '\0';
+		*arr_size += 1;
+	}
 }
 
 char	**ft_split(const char *s, char *c)
 {
-	char	**arr;
-	char	*next;
-	char	*prev;
-	int		i;
+	char		**arr;
+	int			arr_size;
+	int			length;
+	const char	*start;
 
-	arr = (char **)malloc(sizeof(char *) * count_split((char *)s, c, 0));
-	if (!arr)
-		return (0);
-	arr[count_split((char *)s, c, 0) - 1] = NULL;
-	i = 0;
-	next = pass((char *)s, c);
-	prev = next;
-	while (i < count_split((char *)s, c, 0) - 1)
+	arr = NULL;
+	arr_size = 0;
+	while (*s)
 	{
-		next = ft_skipnext(next, c);
-		arr[i++] = ft_strndup(prev, next);
-		next = pass(next, c);
-		prev = next - 1;
-		prev++;
+		while (is_space_or_tab(*s) && *s)
+			s++;
+		if (*s == '\0')
+			break ;
+		start = s;
+		quote_func(&s, c);
+		length = s - start;
+		put_argument(&arr, &arr_size, length, start);
+		while (*s && (is_space_or_tab(*s) || ft_strchr(c, *s)))
+			s++;
 	}
+	arr = ft_realloc(arr, (arr_size + 1) * sizeof(char *));
+	arr[arr_size] = NULL;
 	return (arr);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ohayek <ohayek@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*   By: beeligul <beeligul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/20 22:17:47 by ohayek            #+#    #+#             */
-/*   Updated: 2023/06/20 22:17:47 by ohayek           ###   ########.fr       */
+/*   Created: 2024/01/02 18:58:46 by beeligul          #+#    #+#             */
+/*   Updated: 2024/01/02 18:58:46 by beeligul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,5 +69,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *));
+char	*ft_strncpy(char *dest, const char *src, int n);
 
 #endif
